abeb2e648c8250863ac919d044b252086a454b6a
Sergio G. Girona <sergioglez_8@hotmail.com>
2024-02-06 16:15:50 +0100

    Add model for repo and controller

---
+++ src/repository/repository.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+export interface Repository<N extends { id: string }> {
+  getAll(): Promise<N[]>;
+  getById(id: string): Promise<N>;
+  create(newData: Omit<N, 'id'>): Promise<N>;
+  update(id: N['id'], newData: Partial<N>): Promise<N>;
+  delete(id: string): Promise<void>;
+  search?({ key, value }: { key: string; value: string }): Promise<N>;
+}
